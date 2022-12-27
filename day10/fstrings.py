49c43173d20857b89aed29389ff5b01c978d3dfa
fx900-fx97@7 <ss365493@gmail.com>
2022-12-27 23:59:52 +0530

    fstring and docstring is the very usefull in python

---
+++ day10/fstrings.py
@@ -0,0 +1,11 @@
+#old approch in python
+
+str="my name is {} and my country is {}"
+name="Sagar" 
+country="India"
+print(str.format(name,country))
+
+# fstring method
+
+str2=f"my name is {name} and i live in {country}"
+print(str2)
---
+++ day9/doc_sring.py
@@ -0,0 +1,6 @@
+def square(num):
+    ''' square of two numbers '''
+    print(num**2)
+
+square(5)
+print(square.__doc__)
