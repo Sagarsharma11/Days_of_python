492e00089f15e60240eb68c262797d0b2165f27f
fx900-fx97@7 <ss365493@gmail.com>
2023-01-26 00:05:57 +0530

    python import

---
+++ day18/imports.py
@@ -0,0 +1,11 @@
+import math
+import math as m
+from math import pi,sqrt
+from file import *
+
+print(math.floor(math.pi))
+print(dir(m))
+print(m.sqrt(16))
+print(pi,sqrt(2333))
+welcome()
+print(sagar)
